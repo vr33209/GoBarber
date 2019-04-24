538e6edb5f3a71d25837d0e258cb9191b59be6c8
Victor Rodrigues <vr33209@gmail.com>
2019-04-24 06:44:40 -0300

    primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: "mysql",
+  host: "127.0.0.1",
+  username: "root",
+  password: "M@il10BR",
+  database: "gonodemodulo2",
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};

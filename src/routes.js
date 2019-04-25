aad0c4a5aa0f40028c59b6d2b0e0dcae97cb2c70
Victor Rodrigues <vr33209@gmail.com>
2019-04-25 14:19:51 -0300

    corrigindo problemas de agendademento

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const multerConfig = require("./config/multer");
-const upload = require("multer")(multerConfig);
 const routes = express.Router();
+
+const multerConfig = require("./config/multer");
+const upload = require("multer")(multerConfig)
 const UserController = require("./app/controllers/UserController");
 
 routes.get("/signup", UserController.create);
-routes.post("/signup", UserController.store);
+routes.post("/signup", upload.single("avatar") , UserController.store);
 module.exports = routes;
